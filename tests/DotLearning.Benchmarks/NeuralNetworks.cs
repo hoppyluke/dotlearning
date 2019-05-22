fd6c9a3e94d1ad329dcea28c1be77d5fb3f3d8f5
Luke Hopton <hoppyluke@users.noreply.github.com>
2019-05-22 22:13:15 +0100

    Update to .NET Core 2.2
    Update all dependencies

--- tests/DotLearning.Benchmarks/NeuralNetworks.cs
+++ tests/DotLearning.Benchmarks/NeuralNetworks.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Attributes.Jobs;
 using DotLearning.Mathematics.LinearAlgebra;
 using MatrixNet = DotLearning.NeuralNetworks.MatrixBased.NeuralNetwork;
 using SimpleNet = DotLearning.NeuralNetworks.Simple.NeuralNetwork;
